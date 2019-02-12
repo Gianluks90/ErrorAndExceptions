a591be6ea99ac50b117197e8b080f19db33ed874
Gianluca Marino <45163497+Gianluks90@users.noreply.github.com>
2019-02-12 15:54:17 +0100

    Initial Commit

---
+++ ErrorsAndExceptions/ExceptionFilters/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ExceptionFilters
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            try
+            {
+                ThrowWithErrorCode(405);
+
+            }
+
+            // La Keyword "when" catcha l'eccezione quando la condizione successiva risulta vera, 
+            // se falsa passa al catch successivo;
+
+            // Exception FILTER
+
+            catch (MyCustomException ex) when (ex.ErrorCode == 405)
+            {
+                Console.WriteLine($"Exception caught with filter {ex.Message} and {ex.ErrorCode}");
+            }
+            catch (MyCustomException ex)
+            {
+                Console.WriteLine($"Exception caught {ex.Message} and {ex.ErrorCode}");
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void ThrowWithErrorCode(int code)
+        {
+            throw new MyCustomException("Error in Foo") { ErrorCode = code };
+        }
+    }
+}
